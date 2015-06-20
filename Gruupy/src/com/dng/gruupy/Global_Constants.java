ee68d3b3db8f8b5f15677347067a333d3be85eb1
Jayadeep KM <kmjayadeep@gmail.com>
2015-06-20 17:22:11 +0530

    server new

--- Gruupy/src/com/dng/gruupy/Global_Constants.java
+++ Gruupy/src/com/dng/gruupy/Global_Constants.java
@@ -3,6 +3,7 @@ package com.dng.gruupy;
 public class Global_Constants {
 
 	//public static final String BASE_URL="http://andriodiosdevelopers.com/groupy/";
-	public static final String BASE_URL="http://tuition24.net/tuition24/";
+//	public static final String BASE_URL="http://tuition24.net/tuition24/";
+	public static final String BASE_URL="http://tuition24.net/grou/groupy/";
 //	public static final String BASE_URL="http://tuition24.net/groupy/";	
 }
